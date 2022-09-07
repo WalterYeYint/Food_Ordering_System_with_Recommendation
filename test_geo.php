31ea640f5d7201fb3c89f6bfc7099d3e1929d837
Kan Yu <WalterYeYint@gmail.com>
2022-09-07 22:24:30 +0630

    feat: display map with locaion as center point; commented geocoding function;

--- test_geo.php
+++ test_geo.php
@@ -17,5 +17,18 @@ function getLatLong($address){
 // print_r(getLatLong("44 E 161st St, Bronx, NY 10451"));
 
 // 16.817382078153482, 96.15690557081344
-print_r(getLatLong("Lotteria, No. G-07, Pearl Condo, Kabar Aye Pagoda Road, Yangon, Myanmar (Burma)"));
+// print_r(getLatLong("Lotteria, No. G-07, Pearl Condo, Kabar Aye Pagoda Road, Yangon, Myanmar (Burma)"));
+
+// 16.81721551836253, 96.156581868968
+// print_r(getLatLong("Pearl Condo C, Kabar Aye Pagoda Road, Yangon, Myanmar (Burma)"));
 ?>
+
+<iframe
+  width="600"
+  height="450"
+  style="border:0"
+  loading="lazy"
+  allowfullscreen
+  referrerpolicy="no-referrer-when-downgrade"
+  src="https://www.google.com/maps/embed/v1/view?key=AIzaSyAC7Rj163G5vNrR5_1AEncatw3OHcjTock&center=16.847,96.1804&zoom=18">
+</iframe>
