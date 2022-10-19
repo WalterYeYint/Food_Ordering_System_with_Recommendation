234b9e030c1beb9ba450dc2f618927d61b3645f2
Kan Yu <WalterYeYint@gmail.com>
2022-10-20 06:05:42 +0630

    feat: reload the model on a specified hour and minute of day; add db file that was in xampp during testing;

--- recommendation_api.py
+++ recommendation_api.py
@@ -4,38 +4,46 @@ import pickle
 import time
 import threading
 from multiprocessing import Process
+import datetime
+import mysql.connector as connection
+import numpy as np
+import os
 
-data = pd.read_csv("Recommendation_System/Recommendation_Model/query_index_to_foodID.csv")
+def read_data_and_model():
+	data = pd.read_csv("Recommendation_System/Recommendation_Model/query_index_to_foodID.csv")
 
-# order_with_total_count = pd.read_csv("Recommendation_System/Recommendation_Model/order_with_total_count.csv")
-# order_with_total_count = order_with_total_count.drop_duplicates(['userID', 'foodName'])
-# order_with_total_count_foodID_pivot = order_with_total_count.pivot(index = 'foodID', columns = 'userID', values = 'has_ordered').fillna(0)
-order_with_total_count_foodID_pivot = pd.read_csv("Recommendation_System/Recommendation_Model/order_with_total_count_pivot_id.csv", index_col='foodID')
+	# order_with_total_count = pd.read_csv("Recommendation_System/Recommendation_Model/order_with_total_count.csv")
+	# order_with_total_count = order_with_total_count.drop_duplicates(['userID', 'foodName'])
+	# order_with_total_count_foodID_pivot = order_with_total_count.pivot(index = 'foodID', columns = 'userID', values = 'has_ordered').fillna(0)
+	order_with_total_count_foodID_pivot = pd.read_csv("Recommendation_System/Recommendation_Model/order_with_total_count_pivot_id.csv", index_col='foodID')
 
-# load the model from disk
-model_knn = pickle.load(open('Recommendation_System/Recommendation_Model/knnpickle_file', 'rb'))
-temp = "Ey yo"
+	# load the model from disk
+	model_knn = pickle.load(open('Recommendation_System/Recommendation_Model/knnpickle_file', 'rb'))
+	return data, order_with_total_count_foodID_pivot, model_knn
 
 app = Flask(__name__)
 
-def update_temp(x):
-	global temp
-	temp = "Here we go " + str(x)
-
 def thread_two_func(n, name):
-	x = 0
+	training_hour = 5
+	training_minute = 55
 	global server
 	global restarted
+	global model_is_retrained
 	while True:
-		print("printing ", x)
-		x += 1
-		time.sleep(3)
-		if x%7 == 0:
-			server.terminate()
-			print("Server terminated !!!")
-			update_temp(x)
-			time.sleep(3)
-			restarted = False
+		current_time = datetime.datetime.now()
+		current_hour = current_time.hour
+		current_minute = current_time.minute
+		print("printing ", current_time)
+		time.sleep(1)
+		if training_hour == current_hour and training_minute == current_minute:
+			if model_is_retrained == False:
+				server.terminate()
+				print("Server terminated !!!")
+				os.system("python Recommendation_System/Recommendation_Model/knn_model.py")
+				time.sleep(5)
+				restarted = False
+		else:
+			model_is_retrained = False
 
 @app.route('/')
 def index():
@@ -57,12 +65,13 @@ if __name__ == "__main__":
 	thread_two = threading.Thread(target=thread_two_func, args=(1,'thread_two'))
 	thread_two.start()
 	restarted = False
-	x = 0
+	model_is_retrained = False
 	while True:
 		if restarted == False:
-			print("Temp value is ", temp)
+			data, order_with_total_count_foodID_pivot, model_knn = read_data_and_model()
 			server = Process(target=app.run)		# debug=False by default
 			server.start()
 			restarted = True
+			model_is_retrained = True
 
 	# app.run(debug=False)
